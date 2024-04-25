c083f1aad2ae68f8e6d165ad754e8a000c776f78
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-04-25 10:59:18 +0300

    ft:fetch-request - fetch video from the pexels api

--- django_project/views.py
+++ django_project/views.py
@@ -11,5 +11,39 @@ def home(request):
             quotes.append(quote_content)
         else:
             quotes.append("Failed to fetch quote")
+            
+  # Fetch random GIF
+    # api_key = 'otZpJ4dwyhheSAr0QaM76oYuMOjCGZJq'
+    api_key = '4ounCQ02Y4DWHYHQQ69ZglThi8c7FtpemrBZ1BC1Vm1E8lA8heRc51xh'
+    
+    endpoint = 'https://api.pexels.com/videos/search'
+    headers = {
+        'Authorization': api_key
+    }
+    params = {
+        'query': 'supercars',
+        'per_page': 9  # Number of car-related videos to fetch
+    }
+    response = requests.get(endpoint, headers=headers, params=params)
+    
+    # Handle the response
+    if response.status_code == 200:
+        video_data = response.json()['videos']
+        video_urls = [video['video_files'][0]['link'] for video in video_data]
+    else:
+        video_urls = []  
+
+    
+    gif_urls=[]
+    for _ in range(10):
+        gif_response = requests.get(f'https://api.giphy.com/v1/gifs/random?api_key={api_key}')
+        if gif_response.status_code == 200:
+            gif_url = gif_response.json()['data']['images']['downsized']['url']
+            gif_urls.append(gif_url)
+        else:
+            # this is like a fallback giphy
+            gif_urls.append("https://media.giphy.com/media/yFQ0ywscgobJK/giphy.gif")  
+    
+
   
-  return render(request, 'templates/index.html', {'quotes':quotes})
+  return render(request, 'templates/index.html', {'quotes':quotes, 'video_urls': video_urls})
